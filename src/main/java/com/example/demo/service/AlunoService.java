860cd101617903700b547d273dd4e61411b2365b
Jenifer Placido <jenifer.sdti@gmail.com>
2023-11-07 13:41:22 -0300

    Projeto Generation

--- src/main/java/com/example/demo/service/AlunoService.java
+++ src/main/java/com/example/demo/service/AlunoService.java
@@ -3,10 +3,12 @@ package com.example.demo.service;
 import com.example.demo.model.Aluno;
 import com.example.demo.repository.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class AlunoService {
     @Autowired
     private AlunoRepository alunoRepository;
