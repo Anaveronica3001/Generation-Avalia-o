608399598c417969bf0c4fc87a24542f992b58b1
Jenifer Placido <jenifer.sdti@gmail.com>
2023-11-07 12:43:11 -0300

    Projeto Generation

--- src/main/java/com/example/demo/AvaliacaoApplication.java
+++ src/main/java/com/example/demo/AvaliacaoApplication.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")}, info =@Info(title = "API PARA CADASTRO DE ALUNOS GENERATION", version = "1", description = "API ALUNOS GENERATION"))
 public class AvaliacaoApplication {
 
 	public static void main(String[] args) {
